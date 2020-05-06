807e371d8ab7c3496d3a8139af777dc93e758675
manik2131 <manik2131@users.noreply.github.com>
2020-05-06 13:56:03 +0530

    update
    code efficient

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -14,7 +14,7 @@ exports.calculate = function(req, res) {
 
     //using + operator 
 
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
